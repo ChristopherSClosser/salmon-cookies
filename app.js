d96e816e52a8a2a447f668a45ff174d460e74c6a
Chris Closser <chrisclosser1@gmail.com>
2017-02-13 14:45:34 -0800

    added sales.html objects defined

---
+++ app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/*<---------------object literals--------------->*/
+// declare global variables at the top
+var storeFirstPike = {
+  //has some properties
+  minCustomersHr: 23,
+  maxCustomersHr: 65,
+  avgCookiesPerCustomer: 6.3,
+  custPerHr: function () {
+    math.random();
+  }
+};
+
+var storeSeaTac = {
+  minCustomersHr: 3,
+  maxCustomersHr: 24,
+  avgCookiesPerCustomer: 1.2
+};
+
+var storeSeaCenter = {
+  minCustomersHr: 11,
+  maxCustomersHr: 38,
+  avgCookiesPerCustomer: 3.7
+};
+
+var storeCapHill = {
+  minCustomersHr: 20,
+  maxCustomersHr: 38,
+  avgCookiesPerCustomer: 3.7
+};
+
+var storeAlki = {
+  minCustomersHr: 2,
+  maxCustomersHr: 16,
+  avgCookiesPerCustomer: 4.6
+};
